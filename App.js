9b7d0846b90840cdec6430a95c8f7d11ac2ca6ce
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2023-05-03 02:02:18 +0300

    Zaman aralığına göre bildiirm gönderemiyor.

--- App.js
+++ App.js
@@ -1,5 +1,88 @@
+// import React, { useEffect } from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import HomeScreen from './screens/homeScreen.js';
+// import RotaBulScreen from './screens/RotaBulScreen.js';
+// import AcilCikisScreen from './screens/AcilCikisScreen.js';
+// import DepremScreen from './screens/DepremScreen.js';
+
+// import PushNotification from 'react-native-push-notification';
+
+// import WebSocketService from './service/WebSocketService.js';
+// import Db from './db/db.js';
+
+// const Stack = createNativeStackNavigator();
+// const db = new Db();
+// const socket = new WebSocketService();
+
+// function App() {
+//   useEffect(() => {
+//     socket.connect();
+
+//     const processReceivedData = async (data) => {
+//       try {
+//         const datetime = data.tarih_saat;
+//         const timeRange = await db.findMatchingTimeRange(datetime, datetime);
+
+//         const time = await db.getTimeRange();
+//         console.log(time);
+        
+//         if (timeRange) {
+//           const imageURI = await db.getImageForTimeRange(timeRange.id);
+
+//           PushNotification.localNotification({
+//             title: 'Yeni Veri Alındı',
+//             message: 'Sunucudan yeni veri alındı: ' + JSON.stringify(data),
+//             bigPictureUrl: imageURI,
+//             playSound: true,
+//             soundName: 'default',
+//             importance: 'high',
+//             priority: 'high',
+//           });
+//         } else {
+//           PushNotification.localNotification({
+//             title: 'Yeni Veri Alındı',
+//             message: 'Sunucudan yeni veri alındı: ' + JSON.stringify(data),
+//             playSound: true,
+//             soundName: 'default',
+//             importance: 'high',
+//             priority: 'high',
+//           });
+//         }
+//       } catch (error) {
+//         console.error('An error occurred:', error);
+//       }
+//     };
+
+//     socket.onNewData((data) => {
+//       console.log('Received data:', data);
+//       processReceivedData(data);
+//     });
+
+//     return () => {
+//       socket.disconnect();
+//     };
+//   }, []);
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         screenOptions={{
+//           headerShown: false,
+//         }}
+//       >
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="RotaBul" component={RotaBulScreen} />
+//         <Stack.Screen name="AcilÇıkış" component={AcilCikisScreen} />
+//         <Stack.Screen name="Deprem" component={DepremScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
+
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/homeScreen.js';
@@ -15,27 +98,13 @@ import Db from './db/db.js';
 const Stack = createNativeStackNavigator();
 const db = new Db();
 const socket = new WebSocketService();
+
 function App() {
   useEffect(() => {
     socket.connect();
 
     const processReceivedData = async (data) => {
-      const datetime = data.tarih_saat;
-      const timeRange = await db.findMatchingTimeRange(datetime, datetime);
-
-      if (timeRange) {
-        const imageURI = await db.getImageForTimeRange(timeRange.id);
-
-        PushNotification.localNotification({
-          title: 'Yeni Veri Alındı',
-          message: 'Sunucudan yeni veri alındı: ' + JSON.stringify(data),
-          bigPictureUrl: imageURI,
-          playSound: true,
-          soundName: 'default',
-          importance: 'high',
-          priority: 'high',
-        });
-      } else {
+      try {
         PushNotification.localNotification({
           title: 'Yeni Veri Alındı',
           message: 'Sunucudan yeni veri alındı: ' + JSON.stringify(data),
@@ -44,6 +113,8 @@ function App() {
           importance: 'high',
           priority: 'high',
         });
+      } catch (error) {
+        console.error('An error occurred:', error);
       }
     };
 
@@ -62,7 +133,6 @@ function App() {
       <Stack.Navigator
         screenOptions={{
           headerShown: false,
-          header: () => null,
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} />
@@ -75,6 +145,3 @@ function App() {
 }
 
 export default App;
-
-
-
