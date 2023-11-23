ac538ebbf3d0a7f9d9cee03ae574b07211884ac7
Ömer Kars <81978907+omerkars78@users.noreply.github.com>
2023-11-23 20:48:31 +0300

    .
    .

--- node_modules/react-native-kontaktio/android/src/main/java/com/artirigo/kontaktio/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.artirigo.kontaktio;
-
-public class Constants {
-    public static final String EXCEPTION = "JAVA_EXCEPTION";
-    public static final String ERROR = "REACT_NATIVE_KONTAKTIO_ERROR";
-
-    public static final String TAG = "com.artirigo.kontaktio";
-}
